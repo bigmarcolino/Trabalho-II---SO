cde4c9ffc0bbac0a556ad7026aaaeac50a0be4ae
Marcus Vinícius <big_marcolino@hotmail.com>
2016-02-11 10:52:42 -0200

    Finalização da política LRU, testado com apenas uma thread

--- so.c
+++ so.c
@@ -3,9 +3,9 @@
 #include <pthread.h>
 #include <unistd.h>
 
-#define N_PAGINAS 5
-#define N_THREADS 2
-#define N_FRAMES 64
+#define N_PAGINAS 10
+#define N_THREADS 1
+#define N_FRAMES 10
 
 #define WSL 4 //working set limit
 #define VAZIO -1 //representa posição vazia no vetor
@@ -20,9 +20,22 @@ void printMP()
 {
     int i;
 
-    for(i = 0; i < next_index; i++)
+    for(i = 0; i < N_FRAMES; i++)
     {
         printf("%d ", mp[i]);
+    }
+
+    printf("\n");
+}
+
+//imprime o vetor de índices de um processo
+void printIndiceMP(int* indiceMP)
+{
+    int i;
+
+    for(i = 0; i < WSL; i++)
+    {
+        printf("%d ", indiceMP[i]);
     }
 
     printf("\n");
@@ -33,11 +46,17 @@ void printMP()
 void* aloca_paginas(void *threadid)
 {
     int *id = (int*)threadid;
-    int i;
+    int i, j, k;
     int qtd_paginas_mp = 0;    
-    int* indiceMP = (int*)malloc(WSL*sizeof(int)); //vetor com os índices
+    int numero_pagina;
+    int contem = 0;
 
-    //colocar -1 no vetor de índices, para indicar que as posições estão vazias
+    //vetor com os índices da MP onde estão as páginas do processo
+    //o valor mais a esquerda desse vetor representa o índice da MP que foi alocado a mais tempo
+    //portanto, indiceMP[0] contém a posição da MP que guarda a página mais antiga
+    int* indiceMP = (int*)malloc(WSL*sizeof(int));
+
+    //inicializa indiceMP com -1, para indicar as posições que estão vazias
     for (i = 0; i < WSL; i++)
     {
         indiceMP[i] = VAZIO;
@@ -46,24 +65,90 @@ void* aloca_paginas(void *threadid)
     srand((unsigned)time(NULL));
 
     //for que controla as tentativas de alocação de página, com delay de 3 segundos
+    //a política de alocação é a seguinte:
+    //1--- um número é gerado aleatoriamente
+    //2--- é verificado se esse número (página) já existe na MP. Caso exista, a MP não é alterada e o vetor de índices é atualizado para indicar qual é o índice da página mais antiga
+    //3--- se o número (página) não existir na MP, haverão duas possibilidades: WSL atingido ou não
+    //4--- se o WSL for atingido, a página mais antiga na MP será substituída pela página nova e o vetor de índices atualizado
+    //5--- caso contrário, a página é colaca na MP, pois há espaço
     for (i = 0; i < N_PAGINAS; i++)
     {   
         pthread_mutex_lock(&mutex);
 
-        printf("Processo %d\n", *id);
-        printf("Qtd paginas %d\n", qtd_paginas_mp);
+        //número de página gerado aleatoriamente
+        numero_pagina = rand() % MAX;
 
-        if(qtd_paginas_mp < WSL)
+        printf("%d\n", numero_pagina);
+
+        //verifica se o número gerado está na memória
+        for (int i = 0; i < qtd_paginas_mp; i++)
         {
-            mp[next_index] = rand() % MAX;
+            int indice_mp = indiceMP[i];
 
-            printf("Valor aleatorio %d\n", mp[next_index]);
-            printf("Indice na MP %d\n\n", next_index);
+            //se estiver na mp, o vetor de índices é atualizado
+            if(mp[indice_mp] == numero_pagina)
+            {
+                contem = 1;
 
-            indiceMP[qtd_paginas_mp] = next_index;
-            next_index++;
-            qtd_paginas_mp++;
-        }        
+                k = 0;
+
+                for (j = 0; j < qtd_paginas_mp; j++)
+                { 
+                    if(indiceMP[j] != indice_mp)
+                    {
+                        indiceMP[k] = indiceMP[j];
+                        k++;   
+                    }
+                    else
+                    {
+                        j++;
+                        indiceMP[k] = indiceMP[j];
+                        k++;
+                    }
+                }
+
+                indiceMP[qtd_paginas_mp-1] = indice_mp;
+
+                break;
+            }
+        }
+
+        //se não está na memória, colocar na memória
+        if(contem == 0)
+        {
+            //reorganiza o vetor indiceMP, quando a quantidade de páginas do processo é igual ao WSL
+            if(qtd_paginas_mp == WSL)
+            {
+                int aux = indiceMP[0];
+
+                k = 0;
+
+                for (j = 1; j < qtd_paginas_mp; j++)
+                { 
+                    indiceMP[k] = indiceMP[j];
+                    k++;   
+                }
+
+                indiceMP[qtd_paginas_mp-1] = aux;
+                mp[aux] = numero_pagina;    
+            }
+            else
+            {
+                //verificar aqui a necessidade de swap out
+                //outra alteração que deve ser feita é, antes de tentar alocar uma página, verificar se está no swap
+
+                mp[next_index] = numero_pagina;
+                indiceMP[qtd_paginas_mp] = next_index;
+                next_index++;
+                qtd_paginas_mp++;
+            }
+        }
+
+        contem = 0;
+
+        printIndiceMP(indiceMP);
+
+        printMP();
 
         pthread_mutex_unlock(&mutex);
 
@@ -81,6 +166,12 @@ int main(int argc, char *argv[])
     pthread_mutex_init(&mutex, NULL);
 
     mp = (int*)malloc(N_FRAMES*sizeof(int));
+
+    //inicializa a MP com -1, para indicar as posições que estão vazias
+    for (i = 0; i < N_FRAMES; i++)
+    {
+        mp[i] = VAZIO;
+    }
 
     //criação das threads, com delay de 3 segundos
     for(i = 0; i < N_THREADS; i++)
@@ -111,8 +202,6 @@ int main(int argc, char *argv[])
             exit(-1);
         }
     }
-
-    printMP();
 
     free(mp);
 
